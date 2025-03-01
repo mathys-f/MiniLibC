17b40e0295621f6d96468b713dab9fd87c0c1546
mathys-f <mathys.fillion@epitech.eu>
2025-03-01 02:19:17 +0100

    🐛 Fix strcspn implementation and update tests for correct return type

--- tests/tests_strcspn.c
+++ tests/tests_strcspn.c
@@ -21,11 +21,15 @@ static void redirect_all_stdout(void)
 static void test_strcspn(const char *str1, const char *str2)
 {
     void *handle = dlopen("libasm.so", RTLD_LAZY);
-    char *(*my_strcspn)(const char *str1, const char *str2) = dlsym(handle, "strcspn");
+    size_t(*my_strcspn)(const char *str1, const char *str2) = dlsym(handle, "strcspn");
 
     cr_assert_neq(handle, NULL);
     cr_assert_neq(my_strcspn, NULL);
-    cr_assert_eq(strcspn(str1, str2), my_strcspn(str1, str2));
+
+    size_t ret = my_strcspn(str1, str2);
+    size_t expected = strcspn(str1, str2);
+    cr_assert_eq(ret, expected);
+
     dlclose(handle);
 }
 
